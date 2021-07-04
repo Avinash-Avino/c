3f3c45cfde01cb4c3534e7c57c39094dc8a7b343
Avinash-Avino <avinashavinash91354@gmail.com>
2021-07-04 16:44:19 +0530

    Updates

---
+++ precache-manifest.c7165281e1189a9ee2512afc2037c355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262da1763978d96d29e8a1a14d1bd6dc",
+    "url": "/c/index.html"
+  },
+  {
+    "revision": "9b76b62f7ab02ae33d1d",
+    "url": "/c/static/css/main.b34a8f13.chunk.css"
+  },
+  {
+    "revision": "95a0fe78bf7fe17d09cf",
+    "url": "/c/static/js/2.caf77b6a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/c/static/js/2.caf77b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b76b62f7ab02ae33d1d",
+    "url": "/c/static/js/main.a740674a.chunk.js"
+  },
+  {
+    "revision": "b5bb38b73c486d2767b4",
+    "url": "/c/static/js/runtime-main.dd7a7eec.js"
+  }
+]);
